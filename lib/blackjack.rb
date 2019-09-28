e2db904fe7f2417778116020af01b267eb759ca2
Gee-Wey (Oct 7, London) Yue <geewey@gmail.com>
2019-09-28 01:09:28 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -28,9 +28,10 @@ def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
-def initial_round(deal_card, deal_card)
+def initial_round
   # code #initial_round here
-  sum = deal_card
+  sum = 0
+  sum += deal_card
   sum += deal_card
   display_card_total(sum)
 end
